cbeca423ebfd957a0d46183af4fd372dd4b0b5da
손병진 <sgyos000@gmail.com>
2022-01-24 23:43:17 +0900

    indent style apply

--- src/screens/DetailScreen/index.tsx
+++ src/screens/DetailScreen/index.tsx
@@ -4,35 +4,36 @@ import { selectItemCountById } from "modules/itemList";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 
-import './DetailScreen.scss';
+import "./DetailScreen.scss";
 
 const DetailScreen: FC = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const params = useParams();
-  const paramsInfo = params['info']?.split('&') || [];
-  const id = paramsInfo[0].split('=')[1];
-  const word = paramsInfo[1].split('=')[1];
-  const input = paramsInfo[2].split('=')[1];
-  const count = useSelector(selectItemCountById(id))
+  const paramsInfo = params["info"]?.split("&") || [];
+  const id = paramsInfo[0].split("=")[1];
+  const word = paramsInfo[1].split("=")[1];
+  const input = paramsInfo[2].split("=")[1];
+  const count = useSelector(selectItemCountById(id));
   const detailInfo = useSelector(selectDetailResult);
 
-  useEffect(()=>{
-    if(id && word && input) {
-      dispatch(detailFetch({
-        id,
-        word,
-      }));
+  useEffect(() => {
+    if (id && word && input) {
+      dispatch(
+        detailFetch({
+          id,
+          word,
+        })
+      );
     } else {
-      navigate('/');
+      navigate("/");
     }
-  }, [params, dispatch, navigate, id, word, input])
+  }, [params, dispatch, navigate, id, word, input]);
 
-  return <div className="pg_detail__wrapper">
+  return (
+    <div className="pg_detail__wrapper">
       <div className="pg_detail__nav">
-        <button onClick={()=>navigate(-1)}>
-          뒤로 가기
-        </button>
+        <button onClick={() => navigate(-1)}>뒤로 가기</button>
       </div>
       <div className="pg_detail__count">
         <span>조회수:</span>
@@ -42,7 +43,8 @@ const DetailScreen: FC = () => {
         <span>URI:</span>
         <span>{`${detailInfo.uri}.${input}`}</span>
       </div>
-    </div>;
+    </div>
+  );
 };
 
 export { DetailScreen };
--- src/screens/SearchScreen/index.tsx
+++ src/screens/SearchScreen/index.tsx
@@ -3,7 +3,7 @@ import { FC, useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-import './SearchScreen.scss';
+import "./SearchScreen.scss";
 
 interface AutoCompleteItem {
   id: string;
@@ -15,60 +15,73 @@ interface AutoCompleteItem {
 const SearchScreen: FC = () => {
   const navigate = useNavigate();
   const itemList = useSelector(selectItemList);
-  const [ input, setInput ] = useState<string>('');
-  const [ autoCompleteItem, setAutoCompleteItem ] = useState<AutoCompleteItem[]>([]);
+  const [input, setInput] = useState<string>("");
+  const [autoCompleteItem, setAutoCompleteItem] = useState<AutoCompleteItem[]>(
+    []
+  );
 
   const createAutoCompleteItems = useCallback(() => {
-    const autoCompleteItems: AutoCompleteItem[] = []
-    itemList.forEach(({id, name, category, searchable, count})=>{
+    const autoCompleteItems: AutoCompleteItem[] = [];
+    itemList.forEach(({ id, name, category, searchable, count }) => {
       searchable.forEach((word) => {
-        if(word.includes(input)){
+        if (word.includes(input)) {
           autoCompleteItems.push({
             id,
             name,
             category,
             word,
-          })
+          });
         }
-      })
-    })
+      });
+    });
     return autoCompleteItems;
-  }, [input, itemList])
+  }, [input, itemList]);
 
   const handleItemClick = (id: string, word: string) => {
     navigate(`/detail/id=${id}&word=${word}&input=${input}`);
-  }
+  };
 
   useEffect(() => {
     setAutoCompleteItem(input.length >= 2 ? createAutoCompleteItems() : []);
   }, [input, createAutoCompleteItems]);
 
-  return <div className="pg_search__wrapper">
-    <div className="pg_search__input">
-      <input
-        value={input}
-        onChange={({target: { value }})=>{
-          setInput(value || '');
-        }}
-      />
-    </div>
-    {autoCompleteItem.length ? <div className="pg_search__auto">
-      <div className="pg_search__auto__header">
-        <span>검색어</span>
-        <span>id</span>
-        <span>name</span>
-        <span>분류</span>
+  return (
+    <div className="pg_search__wrapper">
+      <div className="pg_search__input">
+        <input
+          placeholder="검색어 입력"
+          value={input}
+          onChange={({ target: { value } }) => {
+            setInput(value || "");
+          }}
+        />
       </div>
-      {autoCompleteItem.map(({id, name, category, word}, idx) => {
-        return <div key={idx} className="pg_search__auto__container" onClick={()=>handleItemClick(id, word)}>
-          <span>{word}</span>
-          <span>{id}</span>
-          <span>{name}</span>
-          <span>{category}</span>
+      {autoCompleteItem.length ? (
+        <div className="pg_search__auto">
+          <div className="pg_search__auto__header">
+            <span>검색어</span>
+            <span>id</span>
+            <span>name</span>
+            <span>category</span>
+          </div>
+          {autoCompleteItem.map(({ id, name, category, word }, idx) => {
+            return (
+              <div
+                key={idx}
+                className="pg_search__auto__container"
+                onClick={() => handleItemClick(id, word)}
+              >
+                <span>{word}</span>
+                <span>{id}</span>
+                <span>{name}</span>
+                <span>{category}</span>
+              </div>
+            );
+          })}
         </div>
-      })}
-    </div> : null}
-  </div>;
+      ) : null}
+    </div>
+  );
 };
 
 export { SearchScreen };
